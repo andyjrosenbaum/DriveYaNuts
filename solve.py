da4817db8e7bc94ac0059c22ad7debc78b8af532
Andy Rosenbaum <andros@amazon.com>
2019-11-09 10:56:58 -0500

    list of pieces

---
+++ solve.py
@@ -0,0 +1,10 @@
+# Pieces sorted by lexicographic order of numbers in the widdershins
+# (counter-clockwise) direction.
+
+a = [1, 2, 3, 4, 5, 6]
+b = [1, 2, 5, 6, 3, 4]
+c = [1, 3, 5, 2, 4, 6]
+d = [1, 3, 5, 4, 2, 6]
+e = [1, 4, 2, 3, 5, 6]
+f = [1, 5, 3, 2, 6, 4]
+g = [1, 6, 5, 4, 3, 2]
