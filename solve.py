499feb029104736e3b5dcdafeecc9b63f885c48a
Andy Rosenbaum <andros@amazon.com>
2019-11-09 15:57:31 -0500

    Clean up code for finding starting pairs, and display.

--- solve.py
+++ solve.py
@@ -1,6 +1,3 @@
-# Pieces sorted by lexicographic order of numbers in the widdershins
-# (counter-clockwise) direction.
-import itertools
 
 
 class Nut:
@@ -33,6 +30,15 @@ class Nut:
 
     def __getitem__(self, item):
         return self.numbers[item]
+
+    def __gt__(self, other):
+        return self.name > other.name
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __repr__(self):
+        return self.name
 
 
 def get_open_edges(nut_1, index_1, nut_2, index_2):
@@ -74,40 +80,65 @@ def get_open_edges(nut_1, index_1, nut_2, index_2):
     return left_edge, right_edge
 
 
-def try_center(center_nut, all_nuts):
+def try_center(center_nut, all_nuts, verbose=True):
     """
 
     Args:
-        nut:
-        all_nuts:
+        center_nut: Nut
+        all_nuts: list(Nut)
+        verbose: Print each option tried.
 
     Returns:
-    """
-    set_nuts = set(all_nuts)
-    set_nuts.remove(center_nut)
+        list(Nut, Nut, list(Nut), list(Nut): List of valid combinations of
+            Center piece, Second piece, possible Left edges, possible Right edges.
 
+    >>> a = Nut([1, 2, 3, 4, 5, 6], 'a')
+    >>> b = Nut([1, 2, 5, 6, 3, 4], 'b')
+    >>> c = Nut([1, 3, 5, 2, 4, 6], 'c')
+    >>> d = Nut([1, 3, 5, 4, 2, 6], 'd')
+    >>> e = Nut([1, 4, 2, 3, 5, 6], 'e')
+    >>> f = Nut([1, 5, 3, 2, 6, 4], 'f')
+    >>> g = Nut([1, 6, 5, 4, 3, 2], 'g')
+    >>> all_nuts_list = [a, b, c, d, e, f, g]
+    >>> try_center(a, all_nuts_list, verbose=False)
+    [(a, b, [d, f], [c]), (a, e, [c], [d, f]), (a, f, [b, e], [c])]
+
+
+    """
     possible_pairs = []
 
     # Check for second nut validity
     for n in all_nuts:
-        if n not in set_nuts:
+        if n == center_nut:
             continue
+
+        # Match up on the '1's
         left_edge, right_edge = get_open_edges(
             center_nut, 0, n, 0)
 
-        available_nuts = set_nuts - {n}
-        available_edges = set().union(*(nut.edges_set for nut in available_nuts))
-        left_possible = left_edge in available_edges
-        right_possible = right_edge in available_edges
+        available_nuts = sorted(set(all_nuts) - {center_nut, n})
+
+        # available_edges = set().union(*(nut.edges_set for nut in available_nuts))
+        left_options = []
+        right_options = []
+        for m in available_nuts:
+            if left_edge in m.edges_set:
+                left_options.append(m)
+            if right_edge in m.edges_set:
+                right_options.append(m)
+
+        left_possible = bool(left_options)
+        right_possible = bool(right_options)
         overall_possible = left_possible and right_possible
 
-        print(f'Center {center_nut.name}, Next {n.name}')
-        print(f'Left edge: {left_edge}, Right edge: {right_edge}')
-        print(f'Left possible: {left_possible}, Right possible: {right_possible}')
-        print(f'Overall possible: {left_possible and right_possible}')
-        print('')
+        if verbose:
+            print(f'Center {center_nut.name}, Next {n.name}')
+            print(f'Left edge: {left_edge}, Right edge: {right_edge}')
+            print(f'Left possible: {left_possible}, Right possible: {right_possible}')
+            print(f'Overall possible: {left_possible and right_possible}')
+            print('')
         if overall_possible:
-            possible_pairs.append((center_nut, n))
+            possible_pairs.append((center_nut, n, left_options, right_options))
 
     return possible_pairs
 
@@ -125,10 +156,14 @@ all_nuts_list = [a, b, c, d, e, f, g]
 
 possible_starting_pairs = []
 
-for n in all_nuts_list:
-    possible_starting_pairs.extend(try_center(n, all_nuts_list))
+VERBOSE = False
+# VERBOSE = True
+
+for cn in all_nuts_list:
+    possible_starting_pairs.extend(try_center(cn, all_nuts_list, verbose=VERBOSE))
 
 print('')
+print(f'Found {len(possible_starting_pairs)} possible starting pairs.')
 print('Possible starting paris:')
 for pair in possible_starting_pairs:
-    print(f'\tPair: ({pair[0].name}, {pair[1].name})')
+    print(f'\tPair: ({pair[0].name}, {pair[1].name}), Left: {pair[2]}, Right: {pair[3]}')
